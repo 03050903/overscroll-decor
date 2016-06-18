3721ee292ef9c15e5d4267437343841afdd6cef6
brucetoo <brucetoo14@gmail.com>
2016-06-18 21:38:57 +0800

    fix the bug according to comments by amit

--- liboverscroll/src/main/java/me/everything/android/ui/overscroll/adapters/ViewPagerOverScrollDecorAdapter.java
+++ liboverscroll/src/main/java/me/everything/android/ui/overscroll/adapters/ViewPagerOverScrollDecorAdapter.java
@@ -1,6 +1,5 @@
 package me.everything.android.ui.overscroll.adapters;
 
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 
@@ -16,12 +15,20 @@ import me.everything.android.ui.overscroll.VerticalOverScrollBounceEffectDecorat
  * @see HorizontalOverScrollBounceEffectDecorator
  * @see VerticalOverScrollBounceEffectDecorator
  */
-public class ViewPagerOverScrollDecorAdapter implements IOverScrollDecoratorAdapter {
+public class ViewPagerOverScrollDecorAdapter implements IOverScrollDecoratorAdapter, ViewPager.OnPageChangeListener {
 
     protected final ViewPager mViewPager;
 
+    protected int mLastPagerPosition = 0;
+    protected float mLastPagerScrollOffset;
+
     public ViewPagerOverScrollDecorAdapter(ViewPager viewPager) {
         this.mViewPager = viewPager;
+
+        mViewPager.addOnPageChangeListener(this);
+
+        mLastPagerPosition = mViewPager.getCurrentItem();
+        mLastPagerScrollOffset = 0f;
     }
 
     @Override
@@ -31,25 +38,63 @@ public class ViewPagerOverScrollDecorAdapter implements IOverScrollDecoratorAdap
 
     @Override
     public boolean isInAbsoluteStart() {
-        PagerAdapter adapter = mViewPager.getAdapter();
-        if (null != adapter) {
-            if (mViewPager.getCurrentItem() == 0) {
-                return true;
-            }
-            return false;
-        }
-        return false;
+
+        return mLastPagerPosition == 0 &&
+                mLastPagerScrollOffset == 0f &&
+                !mViewPager.canScrollHorizontally(-1);
+
+        /**
+         * 此处有个bug,当 page = 0 的时候往右滑动viewPager,
+         * 此时出现overScroll效果,但是不抬起手指 向左滑动直到抵达屏幕
+         * 的左边沿,再往右滑动 会发现viewPager在不该出现overScroll的
+         * 时候出现了,并且viewPager的Page 0和1的发生了重叠
+         *
+         * 解决方法
+         * 只有当mLastPagerPosition == 0 ..page = 0
+         * mLastPagerScrollOffset == 0f ..滑动的距离=0
+         * !mViewPager.canScrollHorizontally(-1) 可右滑动的判断 -1表示左
+         */
+//        PagerAdapter adapter = mViewPager.getAdapter();
+//        if (null != adapter) {
+//            if (mViewPager.getCurrentItem() == 0) {
+//                return true;
+//            }
+//            return false;
+//        }
+//        return false;
     }
 
     @Override
     public boolean isInAbsoluteEnd() {
-        PagerAdapter adapter = mViewPager.getAdapter();
-        if (null != adapter && adapter.getCount() > 0) {
-            if (mViewPager.getCurrentItem() == adapter.getCount() - 1) {
-                return true;
-            }
-            return false;
-        }
-        return false;
+
+        return mLastPagerPosition == mViewPager.getAdapter().getCount()-1 &&
+                mLastPagerScrollOffset == 0f &&
+                !mViewPager.canScrollHorizontally(1);
+
+//        PagerAdapter adapter = mViewPager.getAdapter();
+//        if (null != adapter && adapter.getCount() > 0) {
+//            if (mViewPager.getCurrentItem() == adapter.getCount() - 1) {
+//                return true;
+//            }
+//            return false;
+//        }
+//        return false;
     }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+        mLastPagerPosition = position;
+        mLastPagerScrollOffset = positionOffset;
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
 }
